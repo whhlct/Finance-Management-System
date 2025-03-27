7c8d1105b34525143cc3f7c24482ead0f19a089b
whhlct <whhlct22@example.com>
2025-03-27 18:51:39 -0400

    created sample transactions page

--- finance-management-system/ledger/views.py
+++ finance-management-system/ledger/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Transaction
+
 # Create your views here.
+def index(request):
+    transactions = Transaction.objects.all()
+    context = {
+        "transactions": transactions,
+        "transaction_count": transactions.count()
+    }
+
+    return render(request, "ledger/index.html", context)
